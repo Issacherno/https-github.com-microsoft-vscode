jessecomeau87/VSCode

Fix advanced line wrap if decorations are present
Decorations may affect line wrapping. For example, they make make the
text bold, or increase or decrease font size. This wasn’t originally
taken into account to calculate the the break offsets.

src/vs/editor/browser/widget/diffEditor/components/diffEditorViewZones/diffEditorViewZones.ts
@@ -175,7 +175,7 @@ export class DiffEditorViewZones extends Disposable {
 				}
 			}
 
-			const lineBreakData = deletedCodeLineBreaksComputer?.finalize() ?? [];
+			const lineBreakData = deletedCodeLineBreaksComputer?.finalize(this._editors.original.getModel()?.getAllDecorations() ?? []) ?? [];
 			let lineBreakDataIdx = 0;
 
 			const modLineHeight = this._editors.modified.getOption(EditorOption.lineHeight);

Issacherno/https-github.com-microsoft-vscode

src/vs/editor/browser/widget/diffEditor/components/diffEditorViewZones/diffEditorViewZones.ts
@@ -175,7 +175,7 @@ export class DiffEditorViewZones extends Disposable {
 				}
 			}
 
-			const lineBreakData = deletedCodeLineBreaksComputer?.finalize() ?? [];
+			const lineBreakData = deletedCodeLineBreaksComputer?.finalize(this._editors.original.getModel()?.getAllTextDecorations() ?? []) ?? [];
 			let lineBreakDataIdx = 0;
 
 			const modLineHeight = this._editors.modified.getOption(EditorOption.lineHeight);